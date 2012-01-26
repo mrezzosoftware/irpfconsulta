1142917f0d03cfe8685d3220836cb615f190a128
mrezzosoftware@gmail.com <mrezzosoftware@gmail.com@8297a9a8-6790-02d3-f79a-4ceb6f85c148>
2012-01-26 03:05:40 +0000

    Criação do HttpClient para acessar o site da receita.

--- trunk/IRPF Consulta/src/com/mrezzosoftware/irpfconsulta/IRPFConsultaMain.java
+++ trunk/IRPF Consulta/src/com/mrezzosoftware/irpfconsulta/IRPFConsultaMain.java
@@ -1,9 +1,25 @@
 package com.mrezzosoftware.irpfconsulta;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -25,11 +41,57 @@ public class IRPFConsultaMain extends Activity {
 			public void onClick(View v) {
 				txtCpf = (EditText) findViewById(R.id.txtCpf);
 				txtCaptcha = (EditText) findViewById(R.id.txtCaptcha);
+				connectTest();
 				
 				Toast.makeText(IRPFConsultaMain.this,
 						String.format("CPF: %s \nCaptcha: %s", txtCpf.getText().toString(), txtCaptcha.getText().toString()),
 						Toast.LENGTH_SHORT).show();
 			}
 		});
+    }
+    
+    private void connectTest() {
+    	
+    	HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet("http://www.receita.fazenda.gov.br");
+		
+    	CookieStore cookieStore = new BasicCookieStore();
+		HttpContext localContext = new BasicHttpContext();
+		localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+		
+		HttpResponse response = null;
+		try {
+			response = httpclient.execute(httpget);
+		} catch (ClientProtocolException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		for (Header h : response.getAllHeaders()) {
+			Log.i("livro", "HName: " + h.getName());
+			Log.i("livro", "HValue: " + h.getValue());
+			
+		}
+		
+		
+		try {
+			response = httpclient.execute(httpget, localContext);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		List<Cookie> cookies = cookieStore.getCookies();
+		
+		for (Cookie c : cookies) {
+			Log.i("livro", "Nome:" + c.getName());
+			Log.i("livro", "Valor:" + c.getValue());
+		}
     }
 }
