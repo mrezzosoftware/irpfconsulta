f02ee52baf17e9d6ea68db4282b48af5a6fcb354
mrezzosoftware@gmail.com <mrezzosoftware@gmail.com@8297a9a8-6790-02d3-f79a-4ceb6f85c148>
2012-01-27 19:41:33 +0000

    Criação da classe de conexão e a classe modelo dos dados do formulário; Ajuste da forma de recarregamento do captcha.

--- IRPF Consulta/src/com/mrezzosoftware/irpfconsulta/IRPFConsultaMain.java
+++ IRPF Consulta/src/com/mrezzosoftware/irpfconsulta/IRPFConsultaMain.java
@@ -1,11 +1,9 @@
 package com.mrezzosoftware.irpfconsulta;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 import org.apache.http.Header;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
@@ -13,7 +11,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.cookie.Cookie;
-import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
@@ -21,8 +18,6 @@ import org.apache.http.protocol.HttpContext;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -30,140 +25,122 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class IRPFConsultaMain extends Activity implements Runnable {
-	
+import com.mrezzosoftware.irpfconsulta.connection.RFConnection;
+
+public class IRPFConsultaMain extends Activity {
+
 	private EditText txtCpf;
+	private Spinner cboAno;
 	private EditText txtCaptcha;
+	private Bitmap captcha;
+	private ImageView imgRecarregar;
 	private ImageButton btConsultar;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.irpfconsultamain);
-        btConsultar = (ImageButton) findViewById(R.id.btnConsultar);
-        btConsultar.setOnClickListener(new View.OnClickListener() {
-			
+	private static final String LOGCAT_TAG = "irpf";
+	private HttpClient httpclient;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.irpfconsultamain);
+		
+		cboAno = (Spinner) findViewById(R.id.cboAno);
+		//cboAno.setAdapter(new Array)
+		
+		
+		btConsultar = (ImageButton) findViewById(R.id.btnConsultar);
+		btConsultar.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				txtCpf = (EditText) findViewById(R.id.txtCpf);
 				txtCaptcha = (EditText) findViewById(R.id.txtCaptcha);
 
-				//new Thread(IRPFConsultaMain.this).start();
-				try {
-					verCaptcha();
-				} catch (ClientProtocolException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				Toast.makeText(IRPFConsultaMain.this,
-						String.format("CPF: %s \nCaptcha: %s", txtCpf.getText().toString(), txtCaptcha.getText().toString()),
+				Toast.makeText(
+						IRPFConsultaMain.this,
+						String.format("CPF: %s \nCaptcha: %s", txtCpf.getText()
+								.toString(), txtCaptcha.getText().toString()),
 						Toast.LENGTH_SHORT).show();
 			}
 		});
-    }
-    
-	public void run() {
-		// TODO Auto-generated method stub
-		try {
-			//connectTest();
-			verCaptcha();
-		} catch (ClientProtocolException e) {
-			Log.e("livro", e.getMessage(), e);
-		} catch (IOException e) {
-			Log.e("livro", e.getMessage(), e);
-		}
-	}
-	
-	private void verCaptcha() throws ClientProtocolException, IOException {
 		
+		imgRecarregar = (ImageView) findViewById(R.id.imgRecarregar);
+		imgRecarregar.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				carregarCaptcha();
+			}
+		});
+
+		carregarCaptcha();
+	}
+
+	// Carrega o captcha do site da receita federal.
+	private void carregarCaptcha() {
 		new Handler().post(new Runnable() {
-			
 			public void run() {
-				
-				HttpClient httpclient = new DefaultHttpClient();
-				HttpGet httpget = new HttpGet("http://www.receita.fazenda.gov.br/scripts/srf/intercepta/captcha.aspx?opt=image");
-				HttpResponse response = null;
-				
-				try {
-					response = httpclient.execute(httpget);
-				} catch (ClientProtocolException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
+
+				RFConnection rfconnection = new RFConnection();
+				captcha = rfconnection
+						.getImage(RFConnection.URL_CAPTCHA_RECEITA);
+
+				if (captcha != null) {
+					ImageView imgView = (ImageView) findViewById(R.id.imgCaptcha);
+					imgView.setImageBitmap(captcha);
+				} else {
+					Toast.makeText(
+							IRPFConsultaMain.this,
+							"Falha ao obter captcha do site da Receita Federal.\nTente novamente em alguns segundos.",
+							Toast.LENGTH_SHORT);
 				}
-				
-				HttpEntity httpEntity = response.getEntity();
-				BufferedHttpEntity bufferedHttpEntity = null;
-				
-				try {
-					bufferedHttpEntity = new BufferedHttpEntity(httpEntity);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				final long tamanhoImagem = bufferedHttpEntity.getContentLength();
-				
-				if (tamanhoImagem > 0) {
-					InputStream is = null;
-					
-					try {
-						is = bufferedHttpEntity.getContent();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-				    Bitmap bitmap = BitmapFactory.decodeStream(is);
-					ImageView imgCaptcha = (ImageView) findViewById(R.id.imgCaptcha);
-				    imgCaptcha.setImageBitmap(bitmap);
-				    imgCaptcha.invalidate();
-				}
-				
-				
 
 			}
 		});
-		
-		
-		
-
 	}
-    
-    private void connectTest() throws ClientProtocolException, IOException {
-    	
-    	HttpClient httpclient = new DefaultHttpClient();
-    	//httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, new HttpHost("cache.fnde.gov.br", 80));
-    	
-    	//HttpGet httpget = new HttpGet("http://www.receita.fazenda.gov.br/Aplicacoes/Atrjo/ConsRest/Atual.app/index.asp");
-    	HttpGet httpget = new HttpGet("http://www.receita.fazenda.gov.br/scripts/srf/intercepta/captcha.aspx?opt=image");
-		
+	
+	
+	
+	// Gerenciamento de cookies, caso necessário. Não apagar ainda.
+	private void connectTest() throws ClientProtocolException, IOException {
+
+		HttpClient httpclient = new DefaultHttpClient();
+		// httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
+		// new HttpHost("cache.fnde.gov.br", 80));
+
+		// HttpGet httpget = new
+		// HttpGet("http://www.receita.fazenda.gov.br/Aplicacoes/Atrjo/ConsRest/Atual.app/index.asp");
+		HttpGet httpget = new HttpGet(
+				"http://www.receita.fazenda.gov.br/scripts/srf/intercepta/captcha.aspx?opt=image");
+
 		Log.i("livro", "MEUREQUEST: " + httpget.getURI());
-		
-    	CookieStore cookieStore = new BasicCookieStore();
-    	
+
+		CookieStore cookieStore = new BasicCookieStore();
+
 		HttpContext localContext = new BasicHttpContext();
 		localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
-		
+
 		HttpResponse response = httpclient.execute(httpget, localContext);
-		//HttpResponse response = httpclient.execute(httpget);
-		
+		// HttpResponse response = httpclient.execute(httpget);
+
 		for (Header h : response.getAllHeaders()) {
 			Log.i("livro", "HName: " + h.getName());
 			Log.i("livro", "HValue: " + h.getValue());
-			
+
 		}
-		
+
 		List<Cookie> cookies = cookieStore.getCookies();
 		for (Cookie c : cookies) {
 			Log.i("livro", "CNome:" + c.getName());
 			Log.i("livro", "CValor:" + c.getValue());
 		}
-    }
+	}
 
+	public static void logError(Throwable error) {
+		Log.e(IRPFConsultaMain.LOGCAT_TAG, error.getMessage(), error);
+	}
+
+	public static void logInfo(String message) {
+		Log.i(IRPFConsultaMain.LOGCAT_TAG, message);
+	}
 }
---
+++ IRPF Consulta/src/com/mrezzosoftware/irpfconsulta/pf/DadosPF.java
@@ -0,0 +1,37 @@
+package com.mrezzosoftware.irpfconsulta.pf;
+
+public class DadosPF {
+	
+	private String cpf;
+	private String ano;
+	private String captcha;
+	
+	public DadosPF() {}
+	
+	public DadosPF(String cpf, String ano, String captcha) {
+		super();
+		this.cpf = cpf;
+		this.ano = ano;
+		this.captcha = captcha;
+	}
+	
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getAno() {
+		return ano;
+	}
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+	public String getCaptcha() {
+		return captcha;
+	}
+	public void setCaptcha(String captcha) {
+		this.captcha = captcha;
+	}
+}
